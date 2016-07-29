df2d0f4c0fd9febd960ae8717706c8ef02dd9c1c
Helmut Tammen <h.tammen@tammen-it-solutions.de>
2016-07-29 08:52:08 +0200

    Updated update_projects script. After npm install it now runs npm update. That way the packages from package.json are updated to the highest wanted version, not the version that is mentioned in package.json

--- scripts/update_project.ts
+++ scripts/update_project.ts
@@ -8,6 +8,8 @@ console.log(`directory ${typings_dir} deleted`);
 
 cp.execSync('npm install');
 console.log('npm install has been executed');
+cp.execSync('npm update');
+console.log('npm update has been executed. Packages are updated to highest wanted version');
 cp.execSync('typings install');
 console.log('typings install has been executed');
 console.log('**********************************');
